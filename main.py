ab02c50b27151f82dd9881e443aadd2aee981682
Eric Ke <keric.yi@gmail.com>
2018-10-13 02:16:23 -0700

    fixed path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from clarifai.rest import ClarifaiApp
+import os
 
 app = ClarifaiApp(api_key='b6fdc0bdb31847798c3568d6922aa2c8')
 # model = app.public_models.general_model
@@ -8,12 +9,15 @@ app = ClarifaiApp(api_key='b6fdc0bdb31847798c3568d6922aa2c8')
 # for concept in concepts:
 #     print(concept['name'], concept['value'])
 
-app.inputs.create_image_from_filename(filename='A_test.jpg', concepts=['A'])
-app.inputs.create_image_from_filename(filename='B_test.jpg', not_concepts=['A'])
+parentDir = os.path.dirname(os.path.realpath('__file__'))
+
+app.inputs.create_image_from_filename(filename= os.path.join(parentDir,'asl_alphabet_test/A_test.jpg'), concepts=['A'])
+app.inputs.create_image_from_filename(filename= os.path.join(parentDir,'asl_alphabet_test/B_test.jpg'), not_concepts=['A'])
+app.models.delete_all()
 model = app.models.create(model_id="ASLAlphabet", concepts=["A"])
 model.train()
 model = app.models.get('ASLAlphabet')
-response = model.predict_by_filename('C_test.jpg')
+response = model.predict_by_filename(os.path.join(parentDir,'asl_alphabet_test/C_test.jpg'))
 concepts = response['outputs'][0]['data']['concepts']
 for concept in concepts:
     print(concept['name'], concept['value'])
