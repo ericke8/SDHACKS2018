4184b4ef32f4d4de18cf08badf925e1f8bbd91f1
Eric Ke <26385966+ericke8@users.noreply.github.com>
2018-10-13 12:59:27 -0700

    update with not concept and include image test from non train

--- main.py
+++ main.py
@@ -19,20 +19,24 @@ appTZ = ClarifaiApp(api_key=api_keys['key3'])
 
 parentDir = os.fsencode(os.path.join(os.path.dirname(os.path.realpath('__file__')), 'asl_alphabet_train'))
 
+testDir = os.fsencode(os.path.join(os.path.dirname(os.path.realpath('__file__')), 'asl_alphabet_test'))
+
 directory = os.fsencode(parentDir)
+
+allLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 's', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 for file in os.listdir(directory):
     fileName = os.fsdecode(file)
+    e = allLetters.index(fileName[:1])
     if fileName[:1] >= 'A' and fileName[:1] <= 'J':
-        appAJ.inputs.create_image_from_filename(filename = os.path.join(directory, os.fsencode(fileName[:1] + '_test.jpg')), concepts=[fileName[:1]])
+        appAJ.inputs.create_image_from_filename(filename = os.path.join(directory, os.fsencode(fileName[:1] + '_test.jpg')), concepts=[fileName[:1]], not_concepts=(allLetters[:e] + allLetters[(e+1):10]))
         print(fileName[:1])
-    if fileName[:1] >= "K" and fileName[:1] <= "s":
-        appKs.inputs.create_image_from_filename(filename = os.path.join(directory, os.fsencode(fileName[:1] + '_test.jpg')), concepts=[fileName[:1]])
+    if fileName[:1] >= "K" and fileName[:1] <= "S" or fileName[:1] == "s":
+        appKs.inputs.create_image_from_filename(filename = os.path.join(directory, os.fsencode(fileName[:1] + '_test.jpg')), concepts=[fileName[:1]], not_concepts=(allLetters[10:e] + allLetters[(e+1):20]))
         print(fileName[:1])
     if fileName[:1] >= "T" and fileName[:1] <= "Z":
-        appTZ.inputs.create_image_from_filename(filename = os.path.join(directory, os.fsencode(fileName[:1] + '_test.jpg')), concepts=[fileName[:1]])
+        appTZ.inputs.create_image_from_filename(filename = os.path.join(directory, os.fsencode(fileName[:1] + '_test.jpg')), concepts=[fileName[:1]], not_concepts=(allLetters[20:e] + allLetters[(e+1):]))
         print(fileName[:1])
-
 
 
 appAJ.models.delete_all()
@@ -59,9 +63,13 @@ model3 = appTZ.models.get('ASLAlphabet3')
 # model5 = app.models.get('ASLAlphabet5')
 # model6 = app.models.get('ASLAlphabet6')
 
-response1 = model1.predict_by_filename(os.path.join(directory,os.fsencode('C_test.jpg')))
-response2 = model2.predict_by_filename(os.path.join(directory,os.fsencode('M_test.jpg')))
-response3 = model3.predict_by_filename(os.path.join(directory,os.fsencode('X_test.jpg')))
+
+response1 = model1.predict_by_filename(os.path.join(testDir,os.fsencode('A61.jpg')))
+response2 = model1.predict_by_filename(os.path.join(testDir,os.fsencode('B2002.jpg')))
+response3 = model1.predict_by_filename(os.path.join(testDir,os.fsencode('C1017.jpg')))
+
+# response2 = model2.predict_by_filename(os.path.join(directory,os.fsencode('M_test.jpg')))
+# response3 = model3.predict_by_filename(os.path.join(directory,os.fsencode('X_test.jpg')))
 # response4 = model4.predict_by_filename(os.path.join(directory,os.fsencode('T_test.jpg')))
 # response5 = model5.predict_by_filename(os.path.join(directory,os.fsencode('V_test.jpg')))
 # response6 = model6.predict_by_filename(os.path.join(directory,os.fsencode('Z_test.jpg')))
