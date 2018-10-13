432cb336a5e528c2bf2aa0345e3d14e3d4dff399
yimingnzhao <90603yahoo@gmail.com>
2018-10-13 14:43:20 -0700

    started start

--- start.py
+++ start.py
@@ -1,19 +1,29 @@
 import sys
 import test
 import test2
+import time
 
-print("Welcome to ASL to Text")
-print("ASL to Text can translate automatically or manaully")
-print("Automatic translation translates every two seconds")
-print("Manual translation requires the SPACE bar to be pressed for each translation")
+print(chr(27) + "[2J")
+time.sleep(0.5)
+print("Welcome to ASL to Text!\n")
+print("ASL to Text has two translation functions")
+print("\t1). Automatic translation translates every two seconds")
+print("\t2). Manual translation SPACE press for each translation")
+print('\n')
+print("Press the <Esc> key to terminate from translation")
+print('\n')
+print("Please make your selection to begin")
+print('\n')
 user_choice = ''
 while True:
-    print('\n')
     user_choice = int(input("Select '0' for automatic and '1' for manual [0/1]: "))
     if user_choice == 0:
         break
     if user_choice == 1:
         break
+    else:
+        print("Your input is invalid")
+        print('\n')
 
 if user_choice == 0:
     test2.startCam()
