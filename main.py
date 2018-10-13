b9d29f8505ca7ae086f4a0a3b33df415c05ad9f8
Eric Ke <keric.yi@gmail.com>
2018-10-13 01:53:14 -0700

    initial test success

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from clarifai.rest import ClarifaiApp
 
 app = ClarifaiApp(api_key='b6fdc0bdb31847798c3568d6922aa2c8')
-model = app.public_models.general_model
-response = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
+# model = app.public_models.general_model
+# response = model.predict_by_url(url='https://samples.clarifai.com/metro-north.jpg')
 
+# concepts = response['outputs'][0]['data']['concepts']
+# for concept in concepts:
+#     print(concept['name'], concept['value'])
+
+app.inputs.create_image_from_filename(filename='A_test.jpg', concepts=['A'])
+app.inputs.create_image_from_filename(filename='B_test.jpg', not_concepts=['A'])
+model = app.models.create(model_id="ASLAlphabet", concepts=["A"])
+model.train()
+model = app.models.get('ASLAlphabet')
+response = model.predict_by_filename('C_test.jpg')
 concepts = response['outputs'][0]['data']['concepts']
 for concept in concepts:
     print(concept['name'], concept['value'])
-
-#app.inputs.create_image_from_url(url='https://samples.clarifai.com/puppy.jpeg', concepts=['A'])
